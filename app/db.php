0e05e45e376e26aa02d6d3704d6a103bff3259e0
bek-vap <jafarbek.rustamov.2010@gmail.com>
2026-08-11 16:31:03 -0400

    Edit db.php

--- app/db.php
+++ app/db.php
@@ -1,7 +1,7 @@
 <?php
 
 function db() {
-    $mysql = new mysqli("localhost", "root", "Jafarbek123000566j", "mvc_food");
+    $mysql = new mysqli("localhost", "root", "password", "mvc_food");
 
     if ($mysql->connect_error) {
         die("DB ulanishda xatolik!");
